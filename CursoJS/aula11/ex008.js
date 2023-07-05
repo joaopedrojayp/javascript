74dba13255877ec0ae1578759a7c7abcc04e904d
João Pedro <135253608+joaopedrojayp@users.noreply.github.com>
2023-07-05 10:05:41 -0300

    atualização

---
+++ CursoJS/aula11/ex008.js
@@ -0,0 +1,14 @@
+//Variavel de velocidade
+var vel = 61
+
+//comentário--->
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+
+//CONDIÇÃO SIMPLES (pois não apresenta a tag "ELSE= se não.")
+if(vel > 60) {
+  console.log(`Você Ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+//Comentário--->
+console.log('Dirija sempre com cinto de segurança!')
---
+++ CursoJS/aula11/ex009.js
@@ -0,0 +1,12 @@
+//VARIAVEL
+var país = 'Brasil'
+console.log(`Vivendo nos ${país}`)
+
+//CONDIÇÃO COMPOSTA
+if(país == 'Brasil') {
+   console.log('Você é Brasileiro!')
+} 
+
+else{
+    console.log('Você é Estrangeiro')
+}
