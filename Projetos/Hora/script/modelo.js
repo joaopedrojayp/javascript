88d209a904f92b524d172155916229db08548e7e
João Pedro <135253608+joaopedrojayp@users.noreply.github.com>
2023-07-10 09:47:39 -0300

    atualizei

--- Projetos/Hora/script/modelo.js
+++
@@ -1,8 +0,0 @@
-function carregar () {
-    var msg = document.querySelector('#msg')
-    var img = document.querySelector('#imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-    
-}
