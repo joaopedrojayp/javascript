bf1f340f56a163d2ca9811a31c3455a7b08a8a73
João Pedro <135253608+joaopedrojayp@users.noreply.github.com>
2023-07-26 10:12:51 -0300

    atualizei

---
+++ CursoJS/aula17/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+
+console.log(`A soma entre as duas variaveis é ${soma(7)}`);
---
+++ CursoJS/aula17/funcao03.js
@@ -0,0 +1,5 @@
+let v = function(x) {
+    return x*2
+}
+
+console.log(`A variavel 2 vezes é igual a ${v(2)}`);
